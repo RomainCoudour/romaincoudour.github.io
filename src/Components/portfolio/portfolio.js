c2818102d58dd355e8b97e61a186bd769285b341
Romain <rom.coudour@gmail.com>
2020-05-09 18:13:51 +0200

    chore(): format repository

--- src/Components/portfolio/portfolio.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Portfolio(props) {
-	return (
-		<section>portfolio</section>
-	)
-}
--- src/Components/resume/resume.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Resume(props) {
-	return (
-		<section>resume</section>
-	)
-}
