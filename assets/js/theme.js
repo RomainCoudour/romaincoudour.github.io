263d06f7021608762a0565fb6b0268fc453d4962
Romain <rom.coudour@gmail.com>
2021-03-07 11:34:05 +0100

    chore(): organize + autoprefixer

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,17 +13,14 @@ function changeTheme() {
 }
 window.addEventListener("theme", () => {
 	const theme = getTheme() || "light";
-	// handle first paint issue
-	setTimeout(() => {
-		const el = document.getElementById("switch-icon");
-		const d =
-			theme === "dark"
-				? // sun
-				  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-				: // moon
-				  "M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z";
-		el.setAttribute("d", d);
-	}, 0);
+	const el = document.getElementById("switch-icon");
+	const d =
+		theme === "dark"
+			? // sun
+			  "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+			: // moon
+			  "M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z";
+	el.setAttribute("d", d);
 	document.documentElement.setAttribute("data-theme", theme);
 });
 if (window.matchMedia) {
